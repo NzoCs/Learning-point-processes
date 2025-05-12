00f67c4b2d573bd8a8ce77af604a7b0a17ba8a17
enzo.regna-guarneri@student-cs.fr <regnaguen@ruche01.cluster>
2025-05-12 17:54:59 +0200

    simulations done

--- main/simulation/run_simulations.sh
+++ main/simulation/run_simulations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --output=err_logs/simul_%A_%a.out
-#SBATCH --error=err_logs/simul_%A_%a.err
+#BATCH --error=err_logs/simul_%A_%a.err
 #SBATCH --partition=gpua100
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
@@ -29,13 +29,13 @@ model=${models[$(( idx / ${#datasets[@]} ))]}
 dataset=${datasets[$(( idx % ${#datasets[@]} ))]}
 
 # Définition du chemin du modèle pour la combinaison actuelle
-model_dir="./checkpoints/${model}/trained_models/${dataset}"
+model_dir="../train/checkpoints/${model}/trained_models/${dataset}"
 
 # Vérification de l'existence du modèle entraîné
-if [ -f "${model_dir}/best.ckpt" ]; then
+if [ "${model_dir}/best.ckpt" ]; then
     echo "Lancement de la simulation pour ${model} sur ${dataset}"
     # Lancement avec srun
-    srun python simulate.py --experiment_id "${model}" --dataset_id "${dataset}"
+    srun python run_simulation.py --experiment_id "${model}" --dataset_id "${dataset}"
 else
     echo "Le fichier ${model_dir}/best.ckpt n'existe pas. Simulation ignorée pour ${model} sur ${dataset}."
 fi

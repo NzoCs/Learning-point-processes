633857066da73d24895852e17423e696ebfe8987
Enzo <enzo.regna-guarneri@student-cs.fr>
2025-05-14 19:28:56 +0200

    maybe fixed devuce bug in simulations

--- easy_tpp/models/basemodel.py
+++ easy_tpp/models/basemodel.py
@@ -563,7 +563,7 @@ class BaseModel(pl.LightningModule, ABC):
         
         # Initialize sequences
         if batch is None :
-            batch = [torch.zeros(batch_size, 2) for _ in range(3)] + [None, None]
+            batch = [torch.zeros(batch_size, 2).to(self.device) for _ in range(3)] + [None, None]
         else :
             batch_size = batch[0].size(0)
         
@@ -582,7 +582,7 @@ class BaseModel(pl.LightningModule, ABC):
         for mark in range(num_mark):
             # Create a mask for each mark separately to avoid broadcasting issues
             mark_mask = (event_seq_label == mark)
-            masked_time_seq = torch.where(mark_mask, time_seq_label, torch.tensor(0.0).to(self.device))
+            masked_time_seq = torch.where(mark_mask.to(self.device), time_seq_label, torch.tensor(0.0).to(self.device))
             marked_last_time_label, _ = masked_time_seq.max(dim=1)
             last_event_time[:,mark] = marked_last_time_label
                     

b7b9411c5c89a5bcfdc027d9a56cd4e765a2ff58
Enzo <enzo.regna-guarneri@student-cs.fr>
2025-05-29 17:21:25 +0200

    fix init file in config_factory

--- easy_tpp/config_factory/__init__.py
+++ easy_tpp/config_factory/__init__.py
@@ -3,7 +3,6 @@ from easy_tpp.config_factory.data_config import DataConfig, TokenizerConfig, Dat
 from easy_tpp.config_factory.hpo_config import HPOConfig, HPORunnerConfig
 from easy_tpp.config_factory.model_config import ModelConfig, BaseConfig
 from easy_tpp.config_factory.runner_config import RunnerConfig
-from easy_tpp.config_factory.distrib_comp_config import DistribCompConfig
 
 __all__ = ['Config',
            'DataConfig',
